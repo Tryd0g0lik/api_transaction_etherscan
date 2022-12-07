7415b43da00e89cc1f561288305d75eeb4fa335b
Tryd0g0lik <work80@mail.ru>
2022-12-07 13:07:16 +0700

    error!

--- web_scraping/confige.py
+++ web_scraping/confige.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
-import os
+# import os
 
-load_dotenv()
-USER=os.getenv("USER")
-PASS=os.getenv("PASS")
+# load_dotenv()
+# USER=os.getenv("USER")
+# PASS=os.getenv("PASS")
+#
+# TOKEN_1 = os.getenv('token_1')
+# API=os.getenv('API')
