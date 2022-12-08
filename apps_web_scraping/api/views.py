bf503c15aef9a6bdb912e087ae76b8d5c3221b3b
Tryd0g0lik <work80@mail.ru>
2022-12-08 08:54:24 +0700

    Insert form with a method post on the main page

--- apps_web_scraping/api/views.py
+++ apps_web_scraping/api/views.py
@@ -1,11 +1,16 @@
 from pprint import pprint
-
+# from forms import GetTokenOfMainForms
 from django.shortcuts import render
-
-# Create your views here.
+from api.forms import GetTokenOfMainForms
 
 def get_tokenView(request):
-	if request.method == 'GET':
-		# pprint(request.GET.__dict__)
-		print(request._messages)
-	return render(request, 'api/index.html', context={})
+	if request.method == 'POST':
+		print(request._post['token_user'])
+		print(request._post['adress_for_search'])
+		print(request._post['method_transactions'])
+		return render(request, 'api/index.html', context={})
+	else:
+		form = GetTokenOfMainForms()
+		return render(request, 'api/index.html', context={'form' : form})
+
+
