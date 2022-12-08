84b8e219e33e2894605e8ed16febfce5f4b154f6
Tryd0g0lik <work80@mail.ru>
2022-12-08 16:39:45 +0700

    README.md were  created

--- apps_web_scraping/api/moduls/functions.py
+++ apps_web_scraping/api/moduls/functions.py
@@ -3,10 +3,6 @@ from datetime import time
 import requests as regs
 from django.utils import dateformat
 
-_API = '5PNDPQY793WFZ3HMK9TC8MNYXI888P84AK'
-ADRESS = '0x118ee078c3625144e3a942566fbc84f187f8c8b6'
-_api = 'api-goerli.etherscan.io/api?module=account&action=txlistinternal&address=%s&startblock=5&endblock=99999999&page=0&offset=5&sort=asc&apikey=%s'\
-	% (ADRESS, _API)
 
 
 def _get_dataApi(
